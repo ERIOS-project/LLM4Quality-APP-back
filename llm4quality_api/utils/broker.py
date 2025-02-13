df8de1523b794b36ddb446b47762c21ea87ee2cf
romain-mezghenna <romain.mezghenna@etu.umontpellier.fr>
2025-02-13 10:04:23 +0100

    +1

--- llm4quality_api/utils/broker.py
+++ llm4quality_api/utils/broker.py
@@ -20,7 +20,7 @@ def consume_messages(queue, callback):
     while True:
         try:
             connection = pika.BlockingConnection(
-                pika.ConnectionParameters(host=Config.RABBITMQ_HOST)
+                pika.ConnectionParameters(host=Config.RABBITMQ_HOST, port=Config.RABBITMQ_PORT, credentials=pika.PlainCredentials(Config.RABBITMQ_USERNAME, Config.RABBITMQ_PASSWORD))
             )
             channel = connection.channel()
             channel.queue_declare(queue=queue, durable=True)
